6730a43ee6585ec2159fa70d7b2fc11ffcccd7b4
Hadise Matloob <hadisematloob@yahoo.com>
2022-06-29 12:56:13 +0430

    get activeTab and setActiveTab from props

--- src/components/Main/MainTabItem.js
+++ src/components/Main/MainTabItem.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const MainTabItem = (props) => {
-    const {data} = props;
+    const {data, activeTab, setActiveTab} = props;
     const {id, icon, title} = data;
-
+    
     return (
         <li>
             <i className={icon}></i>
