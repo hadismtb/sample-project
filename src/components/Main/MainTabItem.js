bb34d51988f587e774cd54d20c176c853b26b4c8
Hadise Matloob <hadisematloob@yahoo.com>
2022-06-29 13:00:01 +0430

    create a function that when clicked on the tab set it as activeTab

--- src/components/Main/MainTabItem.js
+++ src/components/Main/MainTabItem.js
@@ -3,9 +3,13 @@ import React from 'react';
 const MainTabItem = (props) => {
     const {data, activeTab, setActiveTab} = props;
     const {id, icon, title} = data;
-    
+
+    const handleClick = () => {
+        setActiveTab(id)
+    }
+
     return (
-        <li>
+        <li onClick={handleClick}>
             <i className={icon}></i>
             {title}
         </li>
