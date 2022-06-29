4f0e9ef8a21f2d4a58052a521cbbc65b34de47cc
Hadise Matloob <hadisematloob@yahoo.com>
2022-06-29 19:04:11 +0430

    import searchbox component to settings component and put it in settings navbar

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+//COMPONENTS
+import SearchBox from "../shared/SearchBox";
+
 const Settings = () => {
     return (
         <div className='settings'>
             <nav className="settingsNav">
-                
+                <SearchBox/>
             </nav>
         </div>
     );
