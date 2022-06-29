9810eaec63c9b20c88d942d6048a8032b2f35f65
Hadise Matloob <hadisematloob@yahoo.com>
2022-06-29 19:05:45 +0430

    create a state to handling what content displayed

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import { useState } from "react";
 
 //COMPONENTS
 import SearchBox from "../shared/SearchBox";
 
 const Settings = () => {
+    const [shownContent, setShownContent] = useState("default");
+
     return (
         <div className='settings'>
             <nav className="settingsNav">
