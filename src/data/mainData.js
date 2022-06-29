4cbcad6cf56afdb9e2d4cbec89a717c1b06e5089
Hadise Matloob <hadisematloob@yahoo.com>
2022-06-29 12:18:40 +0430

    create an array of our main tab data

---
+++ src/data/mainData.js
@@ -0,0 +1,12 @@
+export const mainTabData = [
+    {
+        icon: "fa-solid fa-bullhorn",
+        title: "Campaigns",
+        id: "campaigns"
+    },
+    {
+        icon: "fa-solid fa-gear",
+        title: "Settings",
+        id: "settings"
+    }
+]
