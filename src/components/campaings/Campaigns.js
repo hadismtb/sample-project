9d46e6c2bcb6e77ee5b86506c84af52e8430c495
Hadise Matloob <hadisematloob@yahoo.com>
2022-06-29 17:31:21 +0430

    import SearchBox component into campaigns and put it on the campaigns navbar

--- src/components/campaings/Campaigns.js
+++ src/components/campaings/Campaigns.js
@@ -5,6 +5,7 @@ import {campaignsTabData} from "../../data/campaignsData";
 
 //COMPONENTS
 import CampaignsTabItem from "./CampaignsTabItem";
+import SearchBox from "../../shared/SearchBox";
 
 const Campaigns = () => {
     const [activeTab, setActiveTab] = useState("");
@@ -17,6 +18,7 @@ const Campaigns = () => {
                         campaignsTabData.map(item => <CampaignsTabItem key={item.id} data={item} setActiveTab={setActiveTab} activeTab={activeTab} />)
                     }
                 </ul>
+                <SearchBox/>
             </nav>
         </div>
     );
