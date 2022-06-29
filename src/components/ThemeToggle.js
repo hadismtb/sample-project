2b4d2a0f18e92b29a2726a85463b35a67bdf2e0e
Hadise Matloob <hadisematloob@yahoo.com>
2022-06-29 20:23:11 +0430

    get theme and setTheme from theme context by using useContext hook

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,8 +1,12 @@
+import { useContext } from "react";
+
 //CONTEXT
 import {ThemeContext} from "../context/ThemeContextProvider";
 
 
 const ThemeToggle = () => {
+    const {theme, setTheme} = useContext(ThemeContext);
+
     return (
         <div>
             
