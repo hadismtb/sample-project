682fb2939d607a0025ac6e5bcb81da3a0c416dda
Hadise Matloob <hadisematloob@yahoo.com>
2022-06-29 19:08:28 +0430

    pass set state of shown content and search content to SearchBox component to handling settings output content

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -5,11 +5,12 @@ import SearchBox from "../shared/SearchBox";
 
 const Settings = () => {
     const [shownContent, setShownContent] = useState("default");
+    const [searchContent, setSearchContent] = useState("");
 
     return (
         <div className='settings'>
             <nav className="settingsNav">
-                <SearchBox/>
+                <SearchBox setActiveTab={setShownContent} setSearchContent={setSearchContent} />
             </nav>
         </div>
     );
