155695f6b4335d926251581e1d776a4aced5b950
Hadise Matloob <hadisematloob@yahoo.com>
2022-06-29 17:02:32 +0430

    import campaigns tab data into campaigns.js component

--- src/components/campaings/Campaigns.js
+++ src/components/campaings/Campaigns.js
@@ -1,11 +1,12 @@
-import React from 'react';
+//DATA
+import {campaignsTabData} from "../../data/campaignsData";
 
 const Campaigns = () => {
     return (
         <div className='campaigns'>
             <nav className="campaignsNav">
                 <ul className="campaignsTabs">
-                    
+
                 </ul>
             </nav>
         </div>
