b91780cd8e35a59c36e8bcd01cdbd5ef7b7a3e10
Hadise Matloob <hadisematloob@yahoo.com>
2022-06-29 20:30:06 +0430

    create an image element with dynamic src that based on theme mode

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -3,13 +3,17 @@ import { useContext } from "react";
 //CONTEXT
 import {ThemeContext} from "../context/ThemeContextProvider";
 
+//IMAGES
+import dark from "../assets/images/dark.png";
+import light from "../assets/images/light.png";
+
 
 const ThemeToggle = () => {
     const {theme, setTheme} = useContext(ThemeContext);
 
     return (
         <div>
-            
+            <img src={theme === 'dark' ? dark : light} alt="theme icon"/>
         </div>
     );
 };
