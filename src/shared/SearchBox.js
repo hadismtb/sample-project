9abd9187be78a7658c5e3cc7a60c0480b52f99d6
Hadise Matloob <hadisematloob@yahoo.com>
2022-06-29 17:45:39 +0430

    create a function for handle changes on input element and set search as activeTab and inputValue as searchContent

--- src/shared/SearchBox.js
+++ src/shared/SearchBox.js
@@ -5,11 +5,21 @@ const SearchBox = (props) => {
 
     const [inputValue, setInputValue] = useState("");
 
+    //FUNCTIONS
+    const handleChange = e => {
+        const target = e.target;
+        setInputValue(target.value);
+        setSearchContent(target.value);
+        if(target.value.trim()) {
+            setActiveTab(target.parentElement.id);
+        }
+    }
+
     return (
         <div className='searchBox'>
             <form id='search'>
                 <i className="fa-solid fa-magnifying-glass"></i>
-                <input type="text" placeholder="search" value={inputValue}/>
+                <input type="text" placeholder="search" value={inputValue} onChange={handleChange}/>
             </form>
         </div>
     );
