f554a210ed460d72189247197105a0a7b003cc0e
Hadise Matloob <hadisematloob@yahoo.com>
2022-06-29 17:01:19 +0430

    create an array that contains campaigns tabs data in campaingsData.js file

---
+++ src/data/campaignsData.js
@@ -0,0 +1,17 @@
+export const campaignsTabData = [
+    {
+        id: "google",
+        icon: "https://img.icons8.com/color/48/000000/google-drive--v2.png",
+        title: "Google"
+    },
+    {
+        id: "hubspot",
+        icon: "https://img.icons8.com/windows/32/000000/hubspot.png",
+        title: "Hubspot"
+    },
+    {
+        id: "pipedrive",
+        icon: "https://img.icons8.com/ios-filled/50/000000/p.png",
+        title: "Pipedrive"
+    }
+]
