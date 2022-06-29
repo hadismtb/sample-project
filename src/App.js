1c2d65e59bb28a55133eaaa77f99d06f202a5c11
Hadise Matloob <hadisematloob@yahoo.com>
2022-06-29 15:49:14 +0430

    wrap MainContent component into settings and campaings component to handling what component showed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import MainTabItem from "./components/Main/MainTabItem";
 
 //DATA
 import { mainTabData } from "./data/mainData";
+import MainContent from "./components/Main/MainContent";
+import Campaigns from "./components/campaings/Campaigns";
+import Settings from "./components/Settings";
 
 function App() {
     const [activeTab, setActiveTab] = useState("");
@@ -15,7 +18,12 @@ function App() {
             {mainTabData.map(item => <MainTabItem key={item.id} data={item} activeTab={activeTab} setActiveTab={setActiveTab} />)}
         </ul>
         <div className="mainOutput">
-
+            <MainContent id="campaigns" activeTab={activeTab}>
+                <Campaigns/>
+            </MainContent>
+            <MainContent id="settings" activeTab={activeTab}>
+                <Settings/>
+            </MainContent>
         </div>
     </div>
   );
