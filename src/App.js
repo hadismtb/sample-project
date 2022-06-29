0a088bde1796cf65b005942b3a05531d30aef69e
Hadise Matloob <hadisematloob@yahoo.com>
2022-06-29 20:12:49 +0430

    wraped app into theme context provider

--- src/App.js
+++ src/App.js
@@ -12,23 +12,28 @@ import { mainTabData } from "./data/mainData";
 //STYLE
 import "./App.css";
 
+//CONTEXT
+import {ThemeContextProvider} from "./context/ThemeContextProvider";
+
 function App() {
     const [activeTab, setActiveTab] = useState("");
 
   return (
-    <div className="app">
-        <ul className="mainTabs">
-            {mainTabData.map(item => <MainTabItem key={item.id} data={item} activeTab={activeTab} setActiveTab={setActiveTab} />)}
-        </ul>
-        <div className="mainOutput">
-            <MainContent id="campaigns" activeTab={activeTab}>
-                <Campaigns/>
-            </MainContent>
-            <MainContent id="settings" activeTab={activeTab}>
-                <Settings/>
-            </MainContent>
+    <ThemeContextProvider>
+        <div className="app">
+            <ul className="mainTabs">
+                {mainTabData.map(item => <MainTabItem key={item.id} data={item} activeTab={activeTab} setActiveTab={setActiveTab} />)}
+            </ul>
+            <div className="mainOutput">
+                <MainContent id="campaigns" activeTab={activeTab}>
+                    <Campaigns/>
+                </MainContent>
+                <MainContent id="settings" activeTab={activeTab}>
+                    <Settings/>
+                </MainContent>
+            </div>
         </div>
-    </div>
+    </ThemeContextProvider>
   );
 }
 
